a8260950c5e2ba834c06278a003fb706b3cdb0c5
shashikantsk2017git-ops <shashikant.sk2017@gmail.com>
2026-06-17 22:06:10 +0530

    Add: valid-tree - submission-6

---
+++ Data Structures & Algorithms/valid-tree/submission-6.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        int edgeCount = edges.length;
+        if(edgeCount+1 != n) return false;
+
+        //Convert Matrix to adjency list
+        List<List<Integer>> adjList = new ArrayList<>();
+        for(int i = 0; i < n; i++) adjList.add(new ArrayList<>());
+        
+        for(int[] arr: edges) {
+            adjList.get(arr[0]).add(arr[1]);
+            adjList.get(arr[1]).add(arr[0]);
+        }
+
+        //Create Vis array to track visited
+        boolean[] vis = new boolean[n];
+        Queue<Integer> que = new LinkedList<>();
+        que.add(0);
+        vis[0] = true;
+        int countVisited = 1;
+
+        while(!que.isEmpty()) {
+            int node = que.poll();
+            for(int child: adjList.get(node)) {
+                if(!vis[child]) {
+                    vis[child] = true;
+                    countVisited++;
+                    que.add(child);
+                }
+            }
+        }
+
+        return n == countVisited;
+    }
+}
