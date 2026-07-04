4ec1cb5f8fe549d602a4054b419fa959277adbff
shashikantsk2017git-ops <shashikant.sk2017@gmail.com>
2026-07-04 19:02:07 +0530

    Add: missing-number - submission-11

---
+++ Data Structures & Algorithms/missing-number/submission-11.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int missingNumber0(int[] nums) {
+        int xor = 0;
+        for(int i = 0; i < nums.length; i++) {
+            xor = xor ^ i ^ nums[i];
+        }
+        return xor ^ nums.length;
+    }
+
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        for(int num: nums) sum = sum ^ num;
+
+        int sumShould = 0;
+        for(int i = 0; i <= nums.length; i++) {
+            sumShould = sumShould ^ i;
+        }
+        return sumShould ^ sum;
+    }
+}
