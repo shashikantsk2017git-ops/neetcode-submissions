f3305430688232379d2cad7d4e434d2c2a96c1a3
shashikantsk2017git-ops <shashikant.sk2017@gmail.com>
2026-07-18 23:57:42 +0530

    Add: non-overlapping-intervals - submission-9

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-9.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] in) {
+        Arrays.sort(in, (a , b) -> a[1] - b[1]);
+
+        int lastTime = in[0][1];
+        int count = 1;
+        for(int i = 1; i < in.length; i++) {
+            if(in[i][0] >= lastTime) {
+                count++;
+                lastTime = in[i][1];
+            }
+        }
+        return in.length - count;
+    }
+    
+}
+
+ 
