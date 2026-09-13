20add66bb5262e198ba6857b7bdd965b5f3bea3e
shashikantsk2017git-ops <shashikant.sk2017@gmail.com>
2026-09-13 23:18:07 +0530

    Add: copy-linked-list-with-random-pointer - submission-8

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-8.java
@@ -0,0 +1,70 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        Node curr = head;
+
+        while (curr != null) {
+            Node node = new Node(curr.val);
+            node.next = curr.next;
+            curr.next = node;
+
+            curr = node.next;
+        }
+
+        curr = head;
+
+        while (curr != null) {
+            if (curr.random == null) {
+                curr.next.random = null;
+            } else {
+                curr.next.random = curr.random.next;
+            }
+            curr = curr.next.next;
+        }
+
+        curr = head;
+        Node newHead = null;
+        if (curr != null)
+            newHead = curr.next;
+        while (curr != null && curr.next != null) {
+            Node temp = curr.next;
+            curr.next = curr.next.next;
+
+            curr = temp;
+        }
+        return newHead;
+    }
+
+    public Node copyRandomList0(Node head) {
+        Map<Node, Node> map = new HashMap<>();
+        Node curr = head;
+        while (curr != null) {
+            map.put(curr, new Node(curr.val));
+            curr = curr.next;
+        }
+
+        curr = head;
+        while (curr != null) {
+            Node node = map.get(curr);
+            node.next = map.get(curr.next);
+            node.random = map.get(curr.random);
+
+            curr = curr.next;
+        }
+        return map.get(head);
+    }
+}
