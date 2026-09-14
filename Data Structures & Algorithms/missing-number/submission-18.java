05417e8597869e42ac71def8ff6da846ac281c75
shashikantsk2017git-ops <shashikant.sk2017@gmail.com>
2026-09-14 23:35:28 +0530

    Add: missing-number - submission-18

---
+++ Data Structures & Algorithms/missing-number/submission-18.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        int arrSum = 0;
+        for(int i = 0; i < nums.length; i++) {
+            sum ^=i;
+            arrSum ^=nums[i];
+        }
+
+        return sum ^ arrSum ^ nums.length;
+    }
+
+    public int missingNumber2(int[] nums) {
+        int sum = 0;
+        int arrSum = 0;
+        for(int i = 0; i <= nums.length; i++) sum ^= i;
+        for(int i = 0; i < nums.length; i++) arrSum ^= nums[i];
+
+        return sum ^ arrSum;
+    }
+
+    public int missingNumber1(int[] nums) {
+        int sum = 0;
+        int arrSum = 0;
+        for(int i = 0; i <= nums.length; i++) sum += i;
+        for(int i = 0; i < nums.length; i++) arrSum += nums[i];
+
+        return sum - arrSum;
+    }
+
+    public int missingNumber0(int[] nums) {
+        Arrays.sort(nums);
+        int n = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != n) return n;
+            n++;
+        }
+        return n;
+    }
+}
