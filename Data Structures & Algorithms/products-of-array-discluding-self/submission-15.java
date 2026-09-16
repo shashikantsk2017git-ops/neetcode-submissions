3b18f8a2a2978aed0098dfdb5dd6527f5eb4459c
shashikantsk2017git-ops <shashikant.sk2017@gmail.com>
2026-09-16 22:36:07 +0530

    Add: products-of-array-discluding-self - submission-15

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-15.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        Arrays.fill(res, 1);
+
+        int prefp = 1;
+        for (int i = 1; i < nums.length; i++) {
+            prefp = prefp * nums[i - 1];
+            res[i] = prefp;
+        }
+
+        int sufp = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            sufp = sufp * nums[i + 1];
+            res[i] = res[i] * sufp;
+        }
+        return res;
+    }
+
+    public int[] productExceptSelf0(int[] nums) {
+        int[] pres = new int[nums.length];
+        int[] sres = new int[nums.length];
+        Arrays.fill(pres, 1);
+        Arrays.fill(sres, 1);
+
+        int ppref = 1;
+        for (int i = 1; i < nums.length; i++) {
+            ppref = ppref * nums[i - 1];
+            pres[i] = ppref;
+        }
+
+        int psuf = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            psuf = psuf * nums[i + 1];
+            sres[i] = psuf;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            pres[i] = pres[i] * sres[i];
+        }
+
+        return pres;
+    }
+}
