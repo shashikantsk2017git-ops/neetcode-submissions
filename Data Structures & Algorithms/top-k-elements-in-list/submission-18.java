7913d3304ac350b8f1ed18602a8eee19a3cbd3bb
shashikantsk2017git-ops <shashikant.sk2017@gmail.com>
2026-09-16 17:09:57 +0530

    Add: top-k-elements-in-list - submission-18

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-18.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int num: nums) map.put(num, map.getOrDefault(num, 0)+1);
+
+        Map<Integer, Integer> sortedMap =  
+        map.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+        .collect(Collectors.toMap(
+            e -> e.getKey(),
+            e -> e.getValue(),
+            (e1, e2) -> e1,
+            () -> new LinkedHashMap<>()
+        ));
+        int[] res = new int[k];
+
+        int i = 0;
+        for(Map.Entry<Integer, Integer> mp: sortedMap.entrySet()) {
+            if(i < k) {
+                res[i] = mp.getKey();
+                i++;
+            }
+        }
+        return res;
+    }
+}
