167043a571061729d3c63cf09045584811e50bed
shashikantsk2017git-ops <shashikant.sk2017@gmail.com>
2026-09-16 19:58:43 +0530

    Add: rotate-matrix - submission-4

---
+++ Data Structures & Algorithms/rotate-matrix/submission-4.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void rotate(int[][] mat) {
+        int n = mat.length-1;
+        for(int i = 0; i <= n; i++) {
+            int l = 0;
+            int r = mat[i].length-1;
+            while(l < r) {
+                int tmp = mat[i][l];
+                mat[i][l] = mat[i][r];
+                mat[i][r] = tmp;
+                l++;
+                r--;
+            }
+        }
+
+        for(int i = 0; i <= n; i++) {
+            for(int j = 0; j <= n; j++) {
+                if(i + j < n) {
+                    int temp = mat[i][j];
+                    mat[i][j] = mat[n-j][n-i];
+                    mat[n-j][n-i] = temp;
+                }
+            }
+        }
+        
+        for(int i = 0; i <= n; i++) {
+            System.out.println(Arrays.toString(mat[i]));
+        }
+    }
+}
