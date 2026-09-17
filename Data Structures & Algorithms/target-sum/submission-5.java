3310a19209662bffc2fab70ce4879202096f78c1
shashikantsk2017git-ops <shashikant.sk2017@gmail.com>
2026-09-17 23:50:58 +0530

    Add: target-sum - submission-5

---
+++ Data Structures & Algorithms/target-sum/submission-5.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        // return countWays(nums, 0, 0, target);
+        int[][] dp = new int[nums.length][1000];
+        for(int i = 0; i < dp.length; i++) Arrays.fill(dp[i], -1);
+        // return countWays(nums, 0, 0, target, dp);
+        return countWays(nums, target);
+    }
+
+    private int countWays(int[] nums, int ind, int sum, int target) {
+        if (ind == nums.length) {
+            return sum == target ? 1 : 0;
+        }
+        // plus
+        int plus = countWays(nums, ind + 1, sum + nums[ind], target);
+
+        // minus
+        int minus = countWays(nums, ind + 1, sum - nums[ind], target);
+
+        return plus + minus;
+    }
+
+    private int countWays(int[] nums, int ind, int sum, int target, int[][] dp) {
+        if (ind == nums.length) {
+            return sum == target ? 1 : 0;
+        }
+        if (dp[ind][sum] != -1)
+            return dp[ind][sum];
+
+        int plus = countWays(nums, ind + 1, sum + nums[ind], target);
+        int minus = countWays(nums, ind + 1, sum - nums[ind], target);
+
+        return dp[ind][sum] = plus + minus;
+    }
+
+    private int countWays(int[] nums, int target) {
+        int n = nums.length;
+
+        // total possible sum range is [-totalSum, totalSum]
+        int totalSum = 0;
+        for (int num : nums) totalSum += num;
+
+        if (Math.abs(target) > totalSum) return 0; // target unreachable
+
+        int offset = totalSum;              // shift so array index is never negative
+        int width = 2 * totalSum + 1;        // valid real sums: -totalSum..totalSum
+
+        int[][] dp = new int[n + 1][width];
+        dp[n][target + offset] = 1;          // base case: real sum == target at ind == n
+
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int sum = width - 1; sum >= 0; sum--) {
+                int plus = (sum + nums[ind] < width) ? dp[ind + 1][sum + nums[ind]] : 0;
+                int minus = (sum - nums[ind] >= 0) ? dp[ind + 1][sum - nums[ind]] : 0;
+                dp[ind][sum] = plus + minus;
+            }
+        }
+
+        return dp[0][offset]; // real sum == 0 at ind == 0
+    }
+}
