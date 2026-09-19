7e1c05beec77cc6da08604937c473c9968202811
shashikantsk2017git-ops <shashikant.sk2017@gmail.com>
2026-09-19 13:30:51 +0530

    Add: reverse-bits - submission-25

---
+++ Data Structures & Algorithms/reverse-bits/submission-25.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int reverseBits(int n) {
+        int res = 0;
+        for(int i = 1; i <= 32; i++) {
+            // int temp = (n & 1) << (32-i);
+            // res = res | temp or res = res + temp;
+            // res |= temp; 
+            res |= (n & 1) << (32-i);
+            n = n >> 1;
+        }
+        return res;
+    }
+
+    public int reverseBits0(int n) {
+        int res = 0;
+        int count = 31;
+        while (count >= 0) {
+            if ((n & 1) == 1) {
+                res = res | (1 << count);
+            }
+            count--;
+            n = n >> 1;
+        }
+        return res;
+    }
+}
