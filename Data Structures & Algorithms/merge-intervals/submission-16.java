e3aa01dede686418bfd1d80eefe66a352da2fb15
shashikantsk2017git-ops <shashikant.sk2017@gmail.com>
2026-09-19 23:04:04 +0530

    Add: merge-intervals - submission-16

---
+++ Data Structures & Algorithms/merge-intervals/submission-16.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[][] merge(int[][] inter) {
+        Arrays.sort(inter, Comparator.<int[]>comparingInt(obj -> obj[0]).thenComparingInt(obj -> obj[1]));
+
+        int[][] res = new int[inter.length][2];
+
+        int ind = 0;
+        int lastTime = inter[0][1];
+        int startTime = inter[0][0];
+
+        res[ind][0] = startTime;
+        res[ind][1] = lastTime;
+        for(int i = 1; i < inter.length; i++) {
+            if(inter[i][0] <= lastTime) {
+                lastTime = Math.max(lastTime, inter[i][1]);
+                startTime = Math.min(startTime, inter[i][0]);
+            } else {
+                lastTime = inter[i][1];
+                startTime = inter[i][0];
+                ind++;
+            }
+            res[ind][0] = startTime;
+            res[ind][1] = lastTime;
+        }
+        return Arrays.copyOfRange(res, 0, ind+1);
+    }
+}
