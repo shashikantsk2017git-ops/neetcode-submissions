b486db38ea2240ecbf5bb6cdcaff195577a6cfca
shashikantsk2017git-ops <shashikant.sk2017@gmail.com>
2026-09-21 21:27:39 +0530

    Add: minimum-window-with-characters - submission-10

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-10.java
@@ -0,0 +1,90 @@
+class Solution {
+
+    public String minWindow(String s, String t) {
+        if (s.length() < t.length()) return "";
+        
+        int[] hash = new int[128];
+        for (char c : t.toCharArray()) hash[c]++;
+        
+        int required = t.length(); // chars still needed
+        int minLen = Integer.MAX_VALUE;
+        int strInd = -1;
+        int i = 0;
+        int count = 0;
+        
+        for (int j = 0; j < s.length(); j++) {
+            if (hash[s.charAt(j)]-- > 0) count++;
+            
+            // shrink from the left while window is still valid
+            while (required == count) {
+                int len = j - i + 1;
+                if (minLen > len) {
+                    minLen = len;
+                    strInd = i;
+                }
+                if (++hash[s.charAt(i)] > 0) count--;
+                i++;
+            }
+        }
+        
+        return strInd == -1 ? "" : s.substring(strInd, strInd + minLen);
+    }
+
+    public String minWindow1(String s, String t) {
+        int[] hash = new int[256];
+        int minLen = Integer.MAX_VALUE;
+        int strInd = -1;
+
+        for(int j = 0; j < t.length(); j++) hash[t.charAt(j)]++;
+
+        for(int i = 0; i < s.length(); i++) {
+            int count = 0;
+            for(int j = i; j < s.length(); j++) {
+                if(hash[s.charAt(j)] > 0) 
+                    count++;
+
+                hash[s.charAt(j)]--;
+                while(count == t.length()) {
+                    if(minLen > (j - i + 1)) {
+                        minLen = j - i + 1;
+                        strInd = i;
+                    }
+                    hash[s.charAt(i)]++;
+                    if(hash[s.charAt(i)] > 0) count--;
+
+                    i++;
+                }
+            }
+        }
+        return strInd == -1 ? "" : s.substring(strInd, strInd + minLen); 
+    }
+
+
+    public String minWindow0(String s, String t) {
+        int minLen = Integer.MAX_VALUE;
+        int strInd = -1;
+
+        for(int i = 0; i < s.length(); i++) {
+
+            int count = 0;
+            int[] hash = new int[256];
+
+            for(int j = 0; j < t.length(); j++) hash[t.charAt(j)]++;
+
+            for(int j = i; j < s.length(); j++) {
+                if(hash[s.charAt(j)]-- > 0) {
+                    count++;
+                }
+                int len = j - i + 1;
+                if(count == t.length()) {
+                    if(minLen > len) {
+                        strInd = i;
+                        minLen = len;
+                    }
+                    break;
+                }
+            }
+        }
+        return strInd == -1 ? "" : s.substring(strInd, strInd + minLen);
+    }
+}
